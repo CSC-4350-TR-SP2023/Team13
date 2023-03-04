a78ffd6e16992fbe6652c3f9923f43d3b7cf371f
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-03-04 01:47:55 -0500

    Create a generateButtonList for each seller

--- buyer.js
+++ buyer.js
@@ -1,14 +1,23 @@
-function generateHTMLList(){
+function generateHTMLList() {
     let seller = ["Ruth Jefferson", "Byron Elliott",
-"Sandra Montes", "Marwa Meadows", "Wilma Cohen", "Hilda Teresita"];
- 
-let list = document.getElementById("myList");
+        "Sandra Montes", "Marwa Meadows", "Wilma Cohen", "Hilda Teresita"];
 
-//create list for each seller
-seller.forEach((item) => {
-  let li = document.createElement("li");
-  li.innerText = item;
-  list.appendChild(li);
-});
+    let list = document.getElementById("seller-list");
+
+    //create list for each seller
+    seller.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        list.appendChild(li);
+    });
 }
+function generateButtonList(){
+    let x = document.querySelectorAll("#seller-list > li");
+    for(let i=0;i<x.length;i++){
+        var btn = document.createElement("button");
+        btn.appendChild(document.createTextNode("View Details"));
+        x[i].appendChild(btn);
+    }
+}
+
 
