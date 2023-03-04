5fca062bc232fda7413a3757348e2af26299d23c
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-03-04 01:29:14 -0500

    Create Js file seller to list function

---
+++ buyer.js
@@ -0,0 +1,11 @@
+let seller = ["Ruth Jefferson", "Byron Elliott",
+"Sandra Montes", "Marwa Meadows", "Wilma Cohen", "Hilda Teresita"];
+ 
+let list = document.getElementById("myList");
+
+//create list for each seller
+seller.forEach((item) => {
+  let li = document.createElement("li");
+  li.innerText = item;
+  list.appendChild(li);
+});
