0bfe556178949f5948ad42c3680ee1c1dad0681f
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-03-05 09:13:46 -0500

    Added some functionality for each click to appear

--- src/buyer.js
+++ src/buyer.js
@@ -4,26 +4,48 @@ function generateHTMLList() {
 
     let list = document.getElementById("seller-list");
 
+    let count = 1;
     //create list for each seller
     seller.forEach((item) => {
         let li = document.createElement("li");
-        li.innerText = item;
+        li.innerText = item + "(10 Miles)";
+        li.setAttribute("id", "seller-" + count);
         list.appendChild(li);
+        count++;
     });
 }
-function generateButtonList() {
-    let x = document.querySelectorAll("#seller-list > li");
-    for (let i = 0; i < x.length; i++) {
-        //Create Button for each list item
-        let btn = document.createElement("button");
-        btn.appendChild(document.createTextNode("View Details"));
-        //Create wrapper for each button
-        let wrapper = document.createElement("div");
-        wrapper.classList.add("button-wrapper");
-        wrapper.appendChild(btn);
+// function generateButtonList() {
+//     let x = document.querySelectorAll("#seller-list > li");
+//     for (let i = 0; i < x.length; i++) {
+//         //Create Button for each list item
+//         let btn = document.createElement("button");
+//         btn.appendChild(document.createTextNode("View Details"));
+//         //Create wrapper for each button
+//         let wrapper = document.createElement("div");
+//         wrapper.classList.add("button-wrapper");
+//         wrapper.appendChild(btn);
 
-        x[i].appendChild(wrapper);
-    }
+//         x[i].appendChild(wrapper);
+//     }
+// }
+function checkForClick(){
+    const sellerList = document.querySelectorAll('li[id^="seller-');
+    sellerList.forEach(sellerListItem => {
+        sellerListItem.addEventListener('click', () => {
+            const sellerName = sellerListItem.innerText;
+            displayName(sellerName)
+        });
+    });
 }
+function displayName(sellerName = ""){
+    const sellerInfo = document.getElementById("seller-info");
 
+    sellerInfo.style.visibility = "visible";
+
+    const header = sellerInfo.querySelector("h2");
+
+    header.innerText = sellerName;
+
+    
+}
 
